3d10b26844301e8a67f1b4ca1aebecb842f0398e
mocarlson <mollyocarlson@gmail.com>
2015-11-01 23:18:28 -0500

    second version Carlson Oct 28 assignment
    This is a revision of the code for my Oct 28 assignment.

---
+++ 006/mollyfirstver2_html (5).php
@@ -0,0 +1,37 @@
+<HTML>
+  <Form method = "Post" action = "">
+        First Name:<input name= "Firstname"/> <br/>
+		Last Name:<input name="lastname"/> <br/>
+			<input type="submit">
+			</form>
+
+<php
+
+echo "Today is " . date("m-d-Y") . "<br>";
+
+echo "Dear Patron,<br/>";
+echo "Today is your lucky day!  Chose a day of the week for free php courses for
+the rest of your life!
+<br/>";
+echo 'This is a list of the days of the week that you may choose from:<br/>';
+
+		  $arr = array("Monday","Tuesday","Wednesday","Thursday","Friday");
+				$arr[0];
+
+for($i=0;$i<5;$i++){
+	echo 'Which will you choose??' .$arr[$i];
+}
+?>
+
+echo "Fill in the name of your favorite php programmer to enter them into a
+contest to win One Million Dollars! </br>";
+<Form method = "POST" action ="">;
+         Name:<input name= "name"/> <br/>
+			<input type="submit">
+
+<php
+echo "Sincerely Yours,<br/>";
+echo "Molly O'Guinness Carlson</br>";
+?>
+
+</HTML>
